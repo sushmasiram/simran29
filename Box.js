48e10ce9a0b3292b50d35a9e9e08ce89ea26d0aa
sushmasiram <67284844+sushmasiram@users.noreply.github.com>
2020-11-07 00:04:25 +0530

    Update Box.js
    Added vanishing property to boxes

--- Box.js
+++ Box.js
@@ -10,17 +10,32 @@ class Box{
   this.height = height;
   
   World.add(world, this.body);
+
+  this.vis = 255
 }
 display(){
   var pos =this.body.position;
   var angle = this.body.angle;
+  
+
+  if(this.body.speed < 3){
+    
+
+  }
+  else{
+  
+  this.vis = this.vis - 5
+  World.remove(world,this.body)
+  }
+  
   push();
   translate(pos.x, pos.y);
   rotate(angle);
   rectMode(CENTER);
   strokeWeight(4);
-  stroke("hotpink");
-  fill("pink");
+  //stroke("hotpink");
+  //fill("pink");
+  fill(0, 153, 204,this.vis)
   rect(0, 0, this.width, this.height);
   pop();
 }
